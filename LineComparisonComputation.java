357b2e1bd0795c22ef66485cdbe294062f31848a
Achintya <achintyatiwari27>
2021-09-03 19:31:18 +0530

    [Refactored]:Code to check line comparison using java comparison method

--- LineComparisonComputation.java
+++ LineComparisonComputation.java
@@ -1,17 +1,28 @@
-package Line_Comparison_Computation;
+package com.BridgeLabz;
 
 import java.util.Scanner;
 
 public class LineComparisonComputation {
+    static int x1; static int x2; static int y1; static int y2;
+    static int a1; static int a2; static int b1; static int b2;
+    static double lengthOfLine1 = 0;
+    static double lengthOfLine2 = 0;
+
+private static void compareTo(double lengthOfLine1, double lengthOfLine2) {
+    lengthOfLine1 = (Math.sqrt(Math.pow((x2-x1),2))+Math.pow((y2-y1) ,2));
+    lengthOfLine2 = (Math.sqrt(Math.pow((a2-a1),2))+Math.pow((b2-b1) ,2));
+    if (lengthOfLine1 == lengthOfLine2) {
+        System.out.println("Both the lines are equal");
+    }else if (lengthOfLine1 > lengthOfLine2) {
+        System.out.println("Length of Line 1 is greater than length of Line 2");
+    }else {
+        System.out.println("Length of line 2 is greater than length of Line 1");
+    }
+}
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation");
-        
-        int x1; int y1; int x2; int y2;
-        int a1; int b1; int a2; int b2;
-        double lengthOfLine1 = 0;
-        double lengthOfLine2 = 0;
 
-      Scanner sc = new Scanner(System.in);
+       Scanner sc = new Scanner(System.in);
         System.out.println("Enter the coordinates for line 1");
         System.out.println("Enter x1");
         x1 = sc.nextInt();
@@ -21,8 +32,6 @@ public class LineComparisonComputation {
         x2 = sc.nextInt();
         System.out.println("Enter y2");
         y2 = sc.nextInt();
-        System.out.println("Length of line " + lengthOfLine1);
-        lengthOfLine1 = (Math.sqrt(Math.pow((x2-x1),2))+Math.pow((y2-y1) ,2));
 
         System.out.println("Enter the coordinates for line 2");
         System.out.println("Enter a1");
@@ -33,15 +42,9 @@ public class LineComparisonComputation {
         a2 = sc.nextInt();
         System.out.println("Enter b2");
         b2 = sc.nextInt();
-        System.out.println("Length of line " + lengthOfLine2);
-        lengthOfLine2 = (Math.sqrt(Math.pow((a2-a1),2))+Math.pow((b2-b1) ,2));
 
-        if (lengthOfLine1 == lengthOfLine2) {
-            System.out.println("Both the lines are equal in length");
-        }
-        else {
-            System.out.println("Both the lines are unequal in length");
-        }
+          compareTo(lengthOfLine1,lengthOfLine2);
     }
 }
+
 
